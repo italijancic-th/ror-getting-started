0f4866ec5a5c0d3f1183642cd166966e29c3eb3d
Ivan Talijancic <ivan.talijancic@teladochealth.com>
2023-01-30 15:46:05 -0300

    Add status for articles and comments
        - Also fix dependency on delete articles. In order to delete
          comments of article at same time.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
+  include Visible
   # Relation to many comments
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # Model data validation
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
